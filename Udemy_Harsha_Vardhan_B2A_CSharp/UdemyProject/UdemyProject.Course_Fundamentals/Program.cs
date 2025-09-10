b4922738e1240c7135bc3cb545c7e838d1870e49
Smit Bhanushali <smitbhanushali27@gmail.com>
2025-09-10 22:56:04 +0530

    add-SystemConsoleClass

--- Udemy_Harsha_Vardhan_B2A_CSharp/UdemyProject/UdemyProject.Course_Fundamentals/Program.cs
+++ Udemy_Harsha_Vardhan_B2A_CSharp/UdemyProject/UdemyProject.Course_Fundamentals/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using UdemyProject.Course_Fundamentals.Hello_World;
+using UdemyProject.Course_Fundamentals.System_Console_Class;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        Hello_Class.Hello_Function();
+        bool runTheProject = false;
+
+        if (runTheProject)
+        {
+            Hello_Class.Hello_Function();
+        }
+        else
+        {
+            SystemConsoleClass.SystemConsoleClassFunction();
+        }
     }
 }
---
+++ Udemy_Harsha_Vardhan_B2A_CSharp/UdemyProject/UdemyProject.Course_Fundamentals/System_Console_Class/SystemConsoleClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyProject.Course_Fundamentals.System_Console_Class
+{
+    public static class SystemConsoleClass
+    {
+        public static void SystemConsoleClassFunction()
+        {
+            Console.WriteLine("Hi");
+            Console.WriteLine("My Name Is");
+            Console.WriteLine("Smit Bhanushali");
+
+            Console.Write("Hi");
+            Console.Write("My Name Is");
+            Console.Write("Smit Bhanushali");
+
+            Console.ReadKey();
+        }
+    }
+}
